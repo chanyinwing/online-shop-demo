2a528a188860a6dfc358a5788f52f29fba63ceeb
[yinwing] <[katuechan@gmail.com]>
2022-07-12 16:50:00 +0800

    css added

--- src/Title.js
+++ src/Title.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function title(props) {
   return (
     <div>        
-        <h1 style={{borderBottom: '5px solid red', textAlign: 'center'}}>{props.mainTitle}</h1>
+        <h1 style={{paddingBottom: '10px', borderBottom: '5px solid red', textAlign: 'center'}}>{props.mainTitle}</h1>
             
     </div>
   )
